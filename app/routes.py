c97e6dc7354324d48b3c400cd6e62cf3b0ebfd5f
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-08-03 20:59:31 -0300

    rr

--- app/routes.py
+++ app/routes.py
@@ -63,59 +63,16 @@ def predict():
             region_southwest = 0
             region_northeast = 1
 
-    
+    values = np.array([[age,sex_male,smoker_yes,bmi,children,region_northwest,region_southeast,region_southwest]])
 
     return render_template('custoplanosauderesultado.html', variavel=nome_da_variavel)
     
     
     """
-        if request.method == 'POST':
-        age = float(request.form['age'])
+        model = pickle.load(open('MedicalInsuranceCost.pkl', 'rb'))
+
+
         
-        
-        sex = request.form['sex']
-        if (sex == 'male'):
-            sex_male = 1
-            sex_female = 0
-        else:
-            sex_male = 0
-            sex_female = 1
-
-        smoker = request.form['smoker']
-        if (smoker == 'yes'):
-            smoker_yes = 1
-            smoker_no = 0
-        else:
-            smoker_yes = 0
-            smoker_no = 1
-
-        bmi = float(request.form['bmi'])
-        children = int(request.form['children'])
-
-        region = request.form['region']
-        if (region == 'northwest'):
-            region_northwest = 1
-            region_southeast = 0
-            region_southwest = 0
-            region_northeast = 0
-        elif (region == 'southeast'):
-            region_northwest = 0
-            region_southeast = 1
-            region_southwest = 0
-            region_northeast = 0
-        elif (region == 'southwest'):
-            region_northwest = 0
-            region_southeast = 0
-            region_southwest = 1
-            region_northeast = 0
-        else:
-            region_northwest = 0
-            region_southeast = 0
-            region_southwest = 0
-            region_northeast = 1
-
-
-        values = np.array([[age,sex_male,smoker_yes,bmi,children,region_northwest,region_southeast,region_southwest]])
         prediction = model.predict(values)
         prediction = round(prediction[0],2)
       
