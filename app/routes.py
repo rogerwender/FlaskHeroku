95a57442f233f1d890705194542bda981b765b44
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-08-03 13:37:03 -0300

    aa

--- app/routes.py
+++ app/routes.py
@@ -68,4 +68,4 @@ def predict():
         prediction = round(prediction[0],2)
 
 
-        return render_template('custoplanosauderesultado.html', prediction_text='A Estimativa de Custo do Plano de Saúde é de R$ {}'.format(prediction))
+        return render_template('contato.html', prediction_text='A Estimativa de Custo do Plano de Saúde é de R$ {}'.format(prediction))
