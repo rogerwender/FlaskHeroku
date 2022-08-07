c92822c524ee9ef9dcccca20bac40300e189069b
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-08-07 12:10:44 -0300

    ss

--- app/static/calculadora.js
+++ app/static/calculadora.js
@@ -1,29 +1,36 @@
-function showTime(){
-    var date = new Date();
-    var h = date.getHours(); 
-    var m = date.getMinutes(); 
-    var s = date.getSeconds(); 
-    var session = "AM";
-    
-    if(h == 0){
-        h = 12;
-    }
-    
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
-    }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("DisplayClock").innerText = time;
-    document.getElementById("DisplayClock").textContent = time;
-    
-    setTimeout(showTime, 1000);
-    
-}
+var limits = 15.0;
 
-showTime();
+$(".card").mousemove(function (e) {
+    var rect = e.target.getBoundingClientRect();
+    var x = e.clientX - rect.left; //x position within the element.
+    var y = e.clientY - rect.top; //y position within the element.
+    var offsetX = x / rect.width;
+    var offsetY = y / rect.height;
+
+    var rotateY = (offsetX) * (limits * 2) - limits;
+    var rotateX = (offsetY) * (limits * 2) - limits;
+
+    var shadowOffsetX = (offsetX) * 32 - 16;
+    var shadowOffsetY = (offsetY) * 32 - 16;
+
+    $(this).css({
+        "box-shadow": (1 / 6) * -shadowOffsetX + "px " + (1 / 6) * -shadowOffsetY + "px 3px rgba(0, 0, 0, 0.051), " +
+            (2 / 6) * -shadowOffsetX + "px " + (2 / 6) * -shadowOffsetY + "px 7.2px rgba(0, 0, 0, 0.073), " +
+            (3 / 6) * -shadowOffsetX + "px " + (3 / 6) * -shadowOffsetY + "px 13.6px rgba(0, 0, 0, 0.09), " +
+            (4 / 6) * -shadowOffsetX + "px " + (4 / 6) * -shadowOffsetY + "px 24.3px rgba(0, 0, 0, 0.107), " +
+            (5 / 6) * -shadowOffsetX + "px " + (5 / 6) * -shadowOffsetY + "px 45.5px rgba(0, 0, 0, 0.129), " +
+            -shadowOffsetX + "px " + -shadowOffsetY + "px 109px rgba(0, 0, 0, 0.18)",
+        transform: "perspective(1000px) rotateX(" + -rotateX + "deg) rotateY(" + rotateY + "deg)"
+    });
+
+    var glarePos = rotateX + rotateY + 90;
+    $(this)
+        .children()
+        .children()
+        .css("left", glarePos + "%");
+});
+
+$(".card").mouseleave(function (e) {
+    $(".card").css({ "box-shadow": "0px 0px 3px rgba(0, 0, 0, 0.051), 0px 0px 7.2px rgba(0, 0, 0, 0.073), 0px 0px 13.6px rgba(0, 0, 0, 0.09), 0px 0px 24.3px rgba(0, 0, 0, 0.107), 0px 0px 45.5px rgba(0, 0, 0, 0.129), 0px 0px 109px rgba(0, 0, 0, 0.18)", "transform": "scale(1.0)" });
+    $(".glare").css("left", "100%");
+});
