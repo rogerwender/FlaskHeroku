82987133e4408878ced88f1dd747f678a078333d
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-08-10 19:02:24 -0300

    ff

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
-from app import app
-from flask import render_template, redirect, request
 import numpy as np
 import pickle
+from app import app
+from flask import render_template, redirect, request
 
-# model = pickle.load(open('MedicalInsuranceCost.pkl', 'rb'))
+ #model = pickle.load(open('MedicalInsuranceCost.pkl', 'rb'))
 
 @app.route('/')
 @app.route('/index')
