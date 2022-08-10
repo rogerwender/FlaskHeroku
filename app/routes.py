daa27911fc80675fb6aaa7a5574ddbb364feadfb
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-08-10 18:58:00 -0300

    ee

--- app/routes.py
+++ app/routes.py
@@ -68,9 +68,9 @@ def predict():
             region_southwest = 0
             region_northeast = 1
 
-        #model = pickle.load(open('MedicalInsuranceCost.pkl', 'rb'))
+        
 
-
+    model = pickle.load(open('MedicalInsuranceCost.pkl', 'rb'))
     values = np.array([[age,sex_male,smoker_yes,bmi,children,region_northwest,region_southeast,region_southwest]])
     #prediction = model.predict(values)
     #prediction = round(prediction[0],2)
