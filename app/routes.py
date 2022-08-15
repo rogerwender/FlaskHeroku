a7e8e165e8d41ba295dae4d36e0bbf5ce2814061
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-08-14 21:55:53 -0300

    fff

--- app/routes.py
+++ app/routes.py
@@ -42,8 +42,8 @@ def calculadora():
     teste = "meu novo valor vai funcionar saporra"
     teste2 = request.form['valor']
     
-    #return render_template('calculadora.html', valor=teste)
-    return render_template('calculadora.html', valor=teste2)
+    return render_template('calculadora.html', valor=teste)
+    #return render_template('calculadora.html', valor=teste2)
 
 @app.route("/predict", methods = ['POST', 'GET'])
 def predict():
