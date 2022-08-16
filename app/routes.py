73b35ecc481bd9471c300cc0334f66b7297050a9
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-08-15 22:26:22 -0300

    aa

--- app/routes.py
+++ app/routes.py
@@ -42,7 +42,7 @@ def previsaofilmesresultado():
 def calculadora():
     if request.method == 'POST':
         valordigitado = request.form.get('valor')
-        s = pyshorteners.Shortener()
+        #s = pyshorteners.Shortener()
         #u = s.tinyurl.short(valordigitado)
         u = "teste"
         return render_template('calculadora.html', meuvalor='O resultado após processamento é ## {}'.format(u))
